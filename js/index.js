6f2787bef71c5cfb4d2d524133508847415e87cc
srtagalicia <srtagalicia@gmail.com>
2020-12-03 20:37:00 +0100

    iteration 2 finished

--- js/index.js
+++ js/index.js
@@ -1,16 +1,31 @@
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
   function startGame() {
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
+    drawRoad();
+    drawCar();
+  }
+
+  function drawRoad() { 
     const roadImage = new Image();
     roadImage.addEventListener('load', function(){
       context.drawImage(roadImage, 0, 0, canvas.width, canvas.height);
     }, false);
     roadImage.src = '../images/road.png';
+  }
+
+  function drawCar() {
+    const carImage = new Image();
+    carImage.addEventListener('load', function(){
+      context.drawImage(carImage, canvas.width/2, canvas.height-120, 50, 100);
+    }, false);
+    carImage.src = '../images/car.png';
   }
 };
 
